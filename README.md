6606b01f53697d9c0399d9289828be39d0269b7e
Wes <=>
2020-05-14 11:27:33 -0400

    docs: ✏️ Add documentation and examples

---
+++ README.md
@@ -0,0 +1,9 @@
+### Arguments
+
+`node index.js input1.csv input2.csv output.csv`
+Output defaults to `output.csv` if argument is not passed in
+
+### Example
+
+`node index.js CSV1.csv CSV2.csv out.csv`
+Outputs items to file `out.csv`
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const fs = require("fs");
 const parse = require("csv-parse/lib/sync");
-const generate = require("csv-stringify/lib/sync");
+const stringify = require("csv-stringify/lib/sync");
 
-const [_prc, _trg, fn1, fn2] = process.argv;
+const [_prc, _trg, fn1, fn2, output = "./output.csv"] = process.argv;
 
 const f1 = fs.readFileSync(fn1);
 const f2 = fs.readFileSync(fn2);
 
-const csv1 = parse(f1, { columns: true }).map((i) => i.email);
-const csv2 = parse(f2, { columns: true }).map((i) => i.email);
+const csv1 = parse(f1, { columns: true });
+const csv2 = parse(f2, { columns: true });
 
 const diff = [];
-csv1.forEach((email) => {
-  if (!csv2.includes(email)) {
-    diff.push({ email, msg: `in ${fn1}` });
+csv1.forEach((row) => {
+  if (!csv2.includes(row)) {
+    diff.push({ ...row, msg: `in ${fn1}` });
   }
 });
 
-csv2.forEach((email) => {
-  if (!csv1.includes(email)) {
-    diff.push({ email, msg: `in ${fn2}` });
+csv2.forEach((row) => {
+  if (!csv1.includes(row)) {
+    diff.push({ ...row, msg: `in ${fn2}` });
   }
 });
 
-fs.writeFileSync("./output.csv", generate(diff));
+fs.writeFileSync(output, stringify(diff));
