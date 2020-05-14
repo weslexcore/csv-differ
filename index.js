925cb3f06d567c6dfb2b43a0b871a139e70260cd
Wes <=>
2020-05-14 11:30:05 -0400

    fix: 🐛 Remove unused dependency

--- index.js
+++ index.js
@@ -7,20 +7,17 @@ const [_prc, _trg, fn1, fn2, output = "./output.csv"] = process.argv;
 const f1 = fs.readFileSync(fn1);
 const f2 = fs.readFileSync(fn2);
 
-const csv1 = parse(f1, { columns: true });
-const csv2 = parse(f2, { columns: true });
+const opts = { columns: true };
+const csv1 = parse(f1, opts);
+const csv2 = parse(f2, opts);
 
 const diff = [];
 csv1.forEach((row) => {
-  if (!csv2.includes(row)) {
-    diff.push({ ...row, msg: `in ${fn1}` });
-  }
+  if (!csv2.includes(row)) diff.push({ ...row, msg: `in ${fn1}` });
 });
 
 csv2.forEach((row) => {
-  if (!csv1.includes(row)) {
-    diff.push({ ...row, msg: `in ${fn2}` });
-  }
+  if (!csv1.includes(row)) diff.push({ ...row, msg: `in ${fn2}` });
 });
 
 fs.writeFileSync(output, stringify(diff));
